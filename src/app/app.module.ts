219a754a90620074bcf584c11a7e7684de0a4ae0
Inkvitop <plinkvitop@gmail.com>
2021-08-04 23:07:34 +0200

    feat: i eslint & fix eslit bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,21 +9,10 @@ import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardComponent } from './components/main/card/card.component';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, HeaderComponent, MainComponent, FooterComponent, CardComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/main/card/card.component.ts
+++ src/app/components/main/card/card.component.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from "@angular/http";
 
-import response from "../../../../assets/response.json";
+import response from '../../../../assets/response.json';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
-
-export class CardComponent implements OnInit {      
+export class CardComponent implements OnInit {
   public data = response;
+
   public list = this.data.items;
 
-  constructor() {
-    
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
